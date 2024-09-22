3702307d0035c38279dbd704b95d4e6ac7c5c5e0
Tarun Jangra <tarunjangra043@gmail.com>
2024-09-22 16:46:30 +0530

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { marked } from "marked";
-import "./App.css";
+import "../styles/App.css";
 
 function App() {
   const [markdown, setMarkdown] = useState("");
